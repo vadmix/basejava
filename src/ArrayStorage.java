b455a4ac340d7686e734e4ac9bddc467ed47d2eb
Vadim Mikhalin <vsmikhalin@gmail.com>
2020-04-06 01:35:06 +0300

    Update classes consistent with a task

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,65 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+	private Resume[] storage = new Resume[10000];
+	private int size;
 
-    void clear() {
-    }
+	void clear() {
+		Arrays.fill(storage, 0, size, null);
+		size = 0;
+	}
 
-    void save(Resume r) {
-    }
+	void save(Resume resume) {
+		for (int r = 0; r < size; r++) {
+			if (resume.uuid == storage[r].uuid) {
+				System.out.println("Такое резюме уже есть");
+				return;
+			}
+		}
+		size++;
+		storage[size - 1] = resume;
+	}
 
-    Resume get(String uuid) {
-        return null;
-    }
+	Resume get(String uuid) {
+		for (int r = 0; r < size; r++) {
+			if (storage[r].uuid == uuid) {
+				return storage[r];
+			}
+		}
+		return null;
+	}
 
-    void delete(String uuid) {
-    }
+	void delete(String uuid) {
+		int deleted = 10000;
+		for (int r = 0; r < size; r++) {
+			if (storage[r].uuid == uuid) {
+				deleted = r;
+			}
+		}
+		if (!(deleted == 10000)) {
+			for (int r = deleted; r < size - 1; r++) {
+				storage[r] = storage[r + 1];
+			}
+			storage[size - 1] = null;
+			size--;
+		}
+	}
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+	/**
+	 * @return array, contains only Resumes in storage (without null)
+	 */
+	Resume[] getAll() {
+		return Arrays.copyOf(storage, size);
+	}
 
-    int size() {
-        return 0;
-    }
+	int size() {
+		return size;
+	}
+
+	void setSize(int size) {
+		this.size = size;
+	}
 }
